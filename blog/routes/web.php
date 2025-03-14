e9d390ee2112db45c54cb39469810a4c96d95ce1
Krishna Luharuka <rahdhekrishnaa@gmail.com>
2025-03-14 11:42:05 +0545

    Integrated food blog template

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +22,13 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/',function(){
+    return view('food_blog.index');
+})->name('index');
+
+Route::get('/about',[AboutController::class,'index'])->name('about');
+
+Route::get('/contact',[ContactController::class,'index'])->name('contact');
+
+Route::get('/categories',[CategoriesController::class,'index'])->name('categories');
